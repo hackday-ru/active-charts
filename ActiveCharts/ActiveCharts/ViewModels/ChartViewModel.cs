0c3fff1cc426954e9477c0062a48e2ca2caa4566
d.nazarov <nazarov.dmirty.a@gmail.com>
2016-04-16 20:31:45 +0300

    new data format applied

---
+++ ActiveCharts/ActiveCharts/ViewModels/ChartViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ActiveCharts.ViewModels
+{
+	public class ChartViewModel
+	{
+		public string Data { get; set; }
+	}
+}
