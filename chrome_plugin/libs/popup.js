43c61f69ac2a5d675371c6c0bc2eada23ffe614f
andrey.demashov <avdemashov_10@yahoo.com>
2016-04-16 22:35:22 +0300

    added plugin functionality

--- chrome_plugin/libs/popup.js
+++ chrome_plugin/libs/popup.js
@@ -13,10 +13,25 @@ Popup.init = function () {
 
         // chrome.tabs.getSelected(null, function(tab) { //<-- "tab" has all the information
         chrome.tabs.query({active: true, currentWindow: true}, function (tab) {
-            chrome.tabs.sendMessage(tab[0].id, {action: "getSelection", url: tab[0].url, token: "56122275dea4f889e8ce24f67034afe"}, function (response) {
-                // console.log(response.requestStatus);
-            });
+            chrome.tabs.sendMessage(tab[0].id, {action: "getSelection", url: tab[0].url, token: "56122275dea4f889e8ce24f67034afe"});
         });
         // });
+    });
+
+    jQuery('#graph-button').click(function () {
+        jQuery.ajax({
+            type: "GET",
+            beforeSend: function() {
+                jQuery('#graph-content').html('');
+                jQuery('.spinner-background').css('display', 'block');
+            },
+            url: 'http://www.cbr.ru/'
+        }).done(function (data) {
+            jQuery('.spinner-background').css('display', 'none');
+            jQuery('#graph-content').append(data);
+        }).fail(function () {
+            jQuery('.spinner-background').css('display', 'none');
+            jQuery('#graph-content').append('<b>Error during loading graph</b>');
+        });
     });
 };
