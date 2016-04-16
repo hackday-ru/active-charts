6fc2ce42c3da60ae1fe37eac8eaffd21ebc04a9d
Dmitry Nazarov <nazarov.dmitry.a@gmail.com>
2016-04-16 14:57:49 +0300

    base controllers + layout

---
+++ ActiveCharts/ActiveCharts/Controllers/BaseController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ActiveCharts.Services.Interfaces;
+
+namespace ActiveCharts.Controllers
+{
+    public class BaseController : Controller
+    {
+        public IAuthService AuthService { get; set; }
+        public IUserService UserService { get; set; }
+
+        public string CurrentUser
+        {
+            get { return AuthService.GetUser(HttpContext); }
+        }
+
+        protected override void Initialize(System.Web.Routing.RequestContext requestContext)
+        {
+            try
+            {
+                base.Initialize(requestContext);
+                if (CurrentUser == null) return;
+                var user = UserService.GetUser(CurrentUser);
+                ViewBag.CurrentUser = user;
+            }
+            catch (Exception ex)
+            {
+                AuthService.LogoutUser(requestContext.HttpContext);
+                ViewBag.CurrentUser = null;
+            }
+
+        }
+    }
+}
---
+++ ActiveCharts/ActiveCharts/Services/UserService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ActiveCharts.Models;
+using ActiveCharts.Services.Interfaces;
+
+namespace ActiveCharts.Services
+{
+    public class UserService : IUserService
+    {
+        public User GetUser(string nickname)
+        {
+            return new User();
+        }
+    }
+}
