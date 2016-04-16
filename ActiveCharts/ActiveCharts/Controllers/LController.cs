b1b36b68b92b347c3ecbaeaa9687736edc7d7f71
r-kamenskiy <r@kamenskiy.net>
2016-04-16 22:04:51 +0300

    l conroller added

---
+++ ActiveCharts/ActiveCharts/Controllers/LController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ActiveCharts.Controllers
+{
+    public class LController : Controller
+    {
+        // GET: L
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
