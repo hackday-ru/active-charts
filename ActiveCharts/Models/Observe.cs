e9fa0dc5e92e8f1218d65a72cd8990414acaff6e
r-kamenskiy <r@kamenskiy.net>
2016-04-16 21:24:28 +0300

    Refactoring

--- ActiveCharts/Models/Observe.cs
+++ ActiveCharts/Models/Observe.cs
@@ -1,13 +1,16 @@
-﻿using MongoDB.Bson;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using MongoDB.Bson.Serialization.Attributes;
 
-namespace DataTracker
+namespace Models
 {
     [BsonIgnoreExtraElements]
-
     public class Observe
     {
-        public string ObserveId{ get; set; }
+        public string ObserveId { get; set; }
         public string UserId { get; set; }
         public string Url { get; set; }
         public string XPath { get; set; }
